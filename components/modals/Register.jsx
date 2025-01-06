9c9a42108920d82add3771592259e372d315b9e9
gokulvasanthJ <gokul27mech@gmail.com>
2025-01-06 12:36:03 +0530

    Initial commit

---
+++ components/modals/Register.jsx
@@ -0,0 +1,136 @@
+"use client";
+import React from "react";
+// import Link from "next/link";
+import axios from "axios";
+import useNavigate from "react-use-navigate";
+
+
+export default function Register() {
+const navigate=useNavigate()
+const handleRegister=async(e)=>{
+e.preventDefault()
+
+
+const data={
+  firstname:e.target.firstname.value,
+  lastname:e.target.lastname.value,
+  email:e.target.email.value,
+  password:e.target.password.value
+  
+}
+try {
+
+  const res=await axios.post(`${process.env.BACK-ENDURL}api/user/register`,data)
+  console.log('hello ');
+
+  if(res.status===200){
+    alert(res.data.message)
+    navigate('/login')
+  }
+  
+} catch (error) {
+  if(error.res){
+    alert(error.res.data.message)
+}
+
+
+}
+
+
+
+
+  return (
+    <div
+      className="modal modalCentered fade form-sign-in modal-part-content"
+      id="register"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="header">
+            <div className="demo-title">Register</div>
+            <span
+              className="icon-close icon-close-popup"
+              data-bs-dismiss="modal"
+            />
+          </div>
+          <div className="tf-login-form">
+            <form onSubmit={handleRegister} className="">
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="text"
+                  required
+                  name="firstname"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  First name
+                </label>
+              </div>
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="text"
+                  required
+                  name="lastname"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  Last name
+                </label>
+              </div>
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="email"
+                  autoComplete="abc@xyz.com"
+                  required
+                  name="email"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  Email *
+                </label>
+              </div>
+              <div className="tf-field style-1">
+                <input
+                  className="tf-field-input tf-input"
+                  placeholder=" "
+                  type="password"
+                  required
+                  name="password"
+                  autoComplete="current-password"
+                />
+                <label className="tf-field-label" htmlFor="">
+                  Password *
+                </label>
+              </div>
+              <div className="bottom">
+                <div className="w-100">
+                  {/* <Link
+                    href={`/register`}
+                    className="tf-btn btn-fill animate-hover-btn radius-3 w-100 justify-content-center"
+                  >
+                    <span>Register</span>
+                  </Link> */}
+                  <button type='submit' className="register">Register</button>
+                </div>
+                <div className="w-100">
+                  <a
+                    href="#login"
+                    data-bs-toggle="modal"
+                    className="btn-link fw-6 w-100 link"
+                  >
+                    Already have an account? Log in here
+                    <i className="icon icon-arrow1-top-left" />
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
